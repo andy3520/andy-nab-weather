c6130861cea10a9d7ee5b1a7d5b31b1105992ec6
An Nguyen Hieu Duc <andy.annguyen3520@gmail.com>
2020-12-29 14:39:04 +0700

    chore: config formatter, linter, sass loader

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'prettier',
+    'prettier/react',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'jest'],
+  env: {
+    browser: true,
+    es6: true,
+    jest: true,
+  },
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    'linebreak-style': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+    // FIX ME: https://github.com/typescript-eslint/typescript-eslint/issues/2077
+    '@typescript-eslint/camelcase': 'off',
+  },
+};
